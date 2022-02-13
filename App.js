56b5de9aaceeabe6c033c911cb2d6134a4b27cb9
thisisjacopo <jacopo.rodighiero@icloud.com>
2022-02-13 20:15:19 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksContainer}>
+        <Text style={styles.tasksTiles}>Today's Tasks:</Text>
+
+        <View style={styles.itemsContainer}>
+          {/* TODOS LISt*/}
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#34aee8",
+  },
+
+  tasksContainer: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  tasksTiles: {
+    color: "#fcfcfd",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  itemsContainer: {
+    marginTop: 40,
   },
 });
---
+++ components/Task.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circle}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#fefcf6",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#6df0be",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  text: {
+    maxWidth: "75%",
+  },
+  circle: {
+    width: 15,
+    height: 15,
+    borderColor: "#4af6b7",
+    borderWidth: 2,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+});
+
+export default Task;
